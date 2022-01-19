ca4be05e0def344b9e2d1ee6dc72a8e0cb5d0261
Kio <un19451024@gmail.com>
2022-01-19 19:17:47 +0900

    Feat: Test 블럭 생성

--- TetrisGame/TetrisGame/Background/Brick.swift
+++ TetrisGame/TetrisGame/Background/Brick.swift
@@ -11,18 +11,23 @@ import SpriteKit
 class Brick {
     
     struct Bricks {
-        var color = UIColor()
-        var points: [CGPoint] = []
         let brickSize: Int = 35
         let zPosition = CGFloat(1)
+        
+        var color = UIColor()
+        // x좌표, y좌표
+        var points: [CGPoint] = []
         var brickName: String = String()
     }
     
-//    func brick() -> Bricks {
-//        var bricks = Bricks()
-//        
-//        var bricks1: [CGPoint] = []
-//        bricks1.append(CGPoint(x: 0, y: 0))
-//        
-//    }
+    func brick() {
+        var bricks: [Bricks] = []
+        
+        var bricks1: [CGPoint] = []
+        bricks1.append(CGPoint(x: 0, y: 0))
+        bricks1.append(CGPoint(x: 1, y: 0))
+        bricks1.append(CGPoint(x: -1, y: 0))
+        bricks1.append(CGPoint(x: 0, y: 1))
+        bricks.append(Bricks(color: .red, points: bricks1, brickName: "brick1"))
+    }
 }
