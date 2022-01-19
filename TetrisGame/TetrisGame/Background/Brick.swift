c07511e084784713702dc0b0fd954b8cdba02b39
Kio <un19451024@gmail.com>
2022-01-19 21:39:06 +0900

    Feat: 7가지 블럭모양 생성

--- TetrisGame/TetrisGame/Background/Brick.swift
+++ TetrisGame/TetrisGame/Background/Brick.swift
@@ -20,14 +20,66 @@ class Brick {
         var brickName: String = String()
     }
     
-    func brick() {
+    func brick() -> Bricks {
         var bricks: [Bricks] = []
         
-        var bricks1: [CGPoint] = []
-        bricks1.append(CGPoint(x: 0, y: 0))
-        bricks1.append(CGPoint(x: 1, y: 0))
-        bricks1.append(CGPoint(x: -1, y: 0))
-        bricks1.append(CGPoint(x: 0, y: 1))
-        bricks.append(Bricks(color: .red, points: bricks1, brickName: "brick1"))
+        // 블럭모양 ㅜ
+        var brick1: [CGPoint] = []
+        brick1.append(CGPoint(x: 0, y: 0))
+        brick1.append(CGPoint(x: 1, y: 0))
+        brick1.append(CGPoint(x: -1, y: 0))
+        brick1.append(CGPoint(x: 0, y: 1))
+        bricks.append(Bricks(color: .red, points: brick1, brickName: "brick1"))
+        
+        // 블럭모양 -
+        var brick2: [CGPoint] = []
+        brick2.append(CGPoint(x: -1, y: 0))
+        brick2.append(CGPoint(x: 0, y: 0))
+        brick2.append(CGPoint(x: 1, y: 0))
+        brick2.append(CGPoint(x: 2, y: 0))
+        bricks.append(Bricks(color: .cyan, points: brick2, brickName: "brick2"))
+        
+        // 블럭모양 ㄱㄴ
+        var brick3: [CGPoint] = []
+        brick3.append(CGPoint(x: 0, y: 0))
+        brick3.append(CGPoint(x: 1, y: 0))
+        brick3.append(CGPoint(x: 0, y: 1))
+        brick3.append(CGPoint(x: -1, y: 1))
+        bricks.append(Bricks(color: .green, points: brick3, brickName: "brick3"))
+        
+        // 블럭모양 ㄴ
+        var brick4: [CGPoint] = []
+        brick4.append(CGPoint(x: -1, y: 1))
+        brick4.append(CGPoint(x: -1, y: 0))
+        brick4.append(CGPoint(x: 0, y: 0))
+        brick4.append(CGPoint(x: 1, y: 0))
+        bricks.append(Bricks(color: .purple, points: brick4, brickName: "brick4"))
+        
+        // 블럭모양 ㅁ
+        var brick5: [CGPoint] = []
+        brick5.append(CGPoint(x: 1, y: 0))
+        brick5.append(CGPoint(x: 0, y: 0))
+        brick5.append(CGPoint(x: 0, y: 1))
+        brick5.append(CGPoint(x: 1, y: 1))
+        bricks.append(Bricks(color: .orange, points: brick5, brickName: "brick5"))
+        
+        // 블럭모양 ⟓
+        var brick6: [CGPoint] = []
+        brick6.append(CGPoint(x: 1, y: 1))
+        brick6.append(CGPoint(x: -1, y: 0))
+        brick6.append(CGPoint(x: 0, y: 0))
+        brick6.append(CGPoint(x: 1, y: 0))
+        bricks.append(Bricks(color: .yellow, points: brick6, brickName: "brick6"))
+        
+        // 블럭모양 ㄱㄴ 반대모양
+        var brick7: [CGPoint] = []
+        brick7.append(CGPoint(x: 0, y: 0))
+        brick7.append(CGPoint(x: -1, y: 0))
+        brick7.append(CGPoint(x: 0, y: 1))
+        brick7.append(CGPoint(x: 1, y: 1))
+        bricks.append(Bricks(color: .systemPink, points: brick7, brickName: "brick7"))
+        
+        let random = Int.random(in: 0..<7)
+        return bricks[random]
     }
 }
